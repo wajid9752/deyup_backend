4b0e8b7c20ec2753cc5ff1275efd9b1ae43a4f4e
developer-wajid <mawazid1051@gmail.com>
2023-10-09 11:14:03 +0530

    Jwt config updated

--- deyup_backend/settings.py
+++ deyup_backend/settings.py
@@ -137,10 +137,10 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 SIMPLE_JWT = {
    
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=90),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
 
